9bef06a23b5ab3bfac229c25c5ea3784df9e05ec
Carlo Di Maio <carlo@tryjeeves.com>
2023-07-18 02:00:14 +0200

    add docker + grafana + tempo

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import config from "config";
 import {getLogger} from "./logger";
 import axios from "axios";
+import {baggageEntryMetadataFromString, context, propagation} from "@opentelemetry/api";
 
 const app: express.Application = express();
 const port = config.get<number>('server.port');
@@ -39,10 +40,21 @@ app.get('/ping', async (_req, _res) => {
     const payload: IEventPayload = {
         timestamp: new Date(),
     };
-    const message: PingMessage = {
-        eventType: EVENT_TYPE.PING,
-        payload,
+
+    const entries = {
+        'cf-ray': { value: 'd4cda95b652f4a1592b449d5929fda1b' },
+        'with/slash': { value: 'with spaces' },
+        key3: { value: 'c88815a7-0fa9-4d95-a1f1-cdccce3c5c2a' },
+        key4: {
+            value: 'foo',
+            metadata: baggageEntryMetadataFromString(
+                'key4prop1=value1;key4prop2=value2;key4prop3WithNoValue',
+            ),
+        },
     };
+
+    const baggage = propagation.createBaggage(entries);
+    propagation.setBaggage(context.active(), baggage);
 
     const pingConfig = config.get<PingConfig>('ping');
     if (pingConfig?.enabled) {
